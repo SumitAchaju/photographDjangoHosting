97f13b16bd1208b7b5c61fefde030362f93a4b17
SumitAchaju <achajusumit@gmail.com>
2023-09-15 22:15:22 +0545

    no vercel

--- build_files.sh
+++
@@ -1,6 +0,0 @@
-python3.9 -m venv env
-cd env/scripts
-activate.bat
-cd ../..
-pip install -r requirements.txt
-python3.9 manage.py runserver
--- photographApi/settings.py
+++ photographApi/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-)x!*nzmv_gsi-%4eo(bh!@gqwd+u@*@3hv6h3%$lb=g=88cj(x
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app' , '.now.sh','127.0.0.1:8000','localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
