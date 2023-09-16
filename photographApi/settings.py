11aba7cc23527de3e71d0fda8935b2958e753bcf
SumitAchaju <achajusumit@gmail.com>
2023-09-16 20:37:27 +0545

    new redis server

--- photographApi/settings.py
+++ photographApi/settings.py
@@ -39,7 +39,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("127.0.0.1",6379)],
+            "hosts": [('redis://vGphoEr4tgofBk2idTg8EmnOlm3Kfxy0@redis-18154.c61.us-east-1-3.ec2.cloud.redislabs.com:18154')],
         }
     },
 }
