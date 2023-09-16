f4cb17538146a62d23ead6a7a77820ce4b223970
SumitAchaju <achajusumit@gmail.com>
2023-09-16 16:14:56 +0545

    new

--- photographApi/asgi.py
+++ photographApi/asgi.py
@@ -10,16 +10,15 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 import os
 
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-from channels.security.websocket import AllowedHostsOriginValidator
-from chat.authmiddleware import JwtAuthMiddlewareStack
-from chat import routing
 
 os.environ.get('DJANGO_SETTINGS_MODULE', 'photographApi.settings')
 
 django_asgi_app = get_asgi_application()
 
-
+from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.security.websocket import AllowedHostsOriginValidator
+from chat.authmiddleware import JwtAuthMiddlewareStack
+from chat import routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
