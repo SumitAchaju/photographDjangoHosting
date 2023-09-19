916e495cf7676ee6477d9612d7ab9e042a430a8c
SumitAchaju <achajusumit@gmail.com>
2023-09-19 08:51:50 +0545

    databse changes

--- photographApi/settings.py
+++ photographApi/settings.py
@@ -136,22 +136,23 @@ WSGI_APPLICATION = "photographApi.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.mysql",
-        "USERNAME": "db-9v8dmreatrb5",
-        "PASSWORD": "WSEk0FjhqKcQPSR0ScFSCZ5o",
-        "HOST": "up-pl-waw1-mysql-1.db.run-on-erla.com",
-        "PORT": 11550,
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
+
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.mysql",
+#         "NAME":"photograph-chat",
+#         "USERNAME": "db-9v8dmreatrb5",
+#         "PASSWORD": "WSEk0FjhqKcQPSR0ScFSCZ5o",
+#         "HOST": "up-pl-waw1-mysql-1.db.run-on-erla.com",
+#         "PORT": 11550,
+#     }
+# }
 
 
 # Password validation
